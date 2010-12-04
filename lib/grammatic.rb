051928cd03dad45b708c6a8e6e0bbacbfd5ea8fc
Bernardo Castro <bernacas@gmail.com>
2010-12-04 04:41:11 -0200

    Implement Deposit & Transfer parser and enhance code

--- lib/grammatic.rb
+++ lib/grammatic.rb
@@ -1,20 +1,48 @@
 require 'treetop'
 
 module Grammatic
+  module Helpers
+    def format_output(result, type = :string)
+      raise "can't be a nil element" if result.nil?
+
+      text_value = result.text_value
+
+      if type == :string
+        text_value.squeeze(" ").strip
+      elsif type == :integer
+        text_value.to_i
+      else
+        raise "can't return a formatted output"
+      end
+    end
+
+    def has_description?(result)
+      result.respond_to?('description') && !result.description.text_value.blank?
+    end
+  end
+
   module Grammar
+    include Grammatic::Helpers
+
     class Transaction < Treetop::Runtime::SyntaxNode
     end
 
     class Expense < Transaction
       def to_json
-        { :tags => self.expense.tags.text_value.squeeze(" ").strip, :amount => self.expense.debit.amount.text_value.to_i }
+        { :account => format_output(expense.account), :tags => format_output(expense.tags), :amount => format_output(expense.debit, :integer) }
       end
     end
 
     class Deposit < Transaction
+      def to_json
+        { :account => format_output(deposit.account), :tags => format_output(deposit.tags), :amount => format_output(deposit.credit, :integer) }
+      end
     end
 
     class Transfer < Transaction
+      def to_json
+        { :from => format_output(transfer.from), :to => format_output(transfer.to), :ammount => format_output(transfer.amount, :integer) }
+      end
     end
 
     class Exchange < Transaction
@@ -22,7 +50,6 @@ module Grammatic
   end
 
   def self.parse(expression)
-    include Grammatic::Grammar
     Treetop.load 'grammar/budgeteer'
 
     parser = BudgeteerGrammarParser.new
@@ -31,8 +58,7 @@ module Grammatic
     raise "The expression is not valid" if result.nil?
 
     parts = result.to_json
-    parts[:account]     = result.expense.account.text_value.squeeze(" ").strip unless result.expense.account.text_value.blank?
-    parts[:description] = result.description.text_value.squeeze(" ").strip unless result.description.text_value.blank?
+    parts[:description] = format_output(result.description) if has_description?(result)
     parts
   end
 end
--- spec/grammatic_spec.rb
+++ spec/grammatic_spec.rb
@@ -9,13 +9,35 @@ describe "Grammatic" do
     describe "giving a valid expression" do
       it "should recognize all the parts of that string" do
         parts = Grammatic.parse("@savings super food 100 some groceries for tonight")
-
         parts.should == { :tags => "super food", :amount => 100, :account => "@savings", :description => "some groceries for tonight" }
       end
 
       it "should regognize minimal correct expression" do
         parts = Grammatic.parse("beer 55")
-        parts.should == { :tags => "beer", :amount => 55 }
+        parts.should == { :tags => "beer", :amount => 55, :account => "" }
+      end
+    end
+  end
+
+  describe Deposit do
+    describe "giving a valid expression" do
+      it "should recognize all the parts of that string" do
+        parts = Grammatic.parse("@savings salary +1000 august income")
+        parts.should == { :account => "@savings", :tags => "salary", :amount => 1000, :description => "august income" }
+      end
+    end
+  end
+
+  describe Transfer do
+    describe "giving a valid expression" do
+      it "should recognize all the parts of that string" do
+        parts = Grammatic.parse("@savings @cash 3500 to pay coworking space")
+        parts.should == { :from => "@savings", :to => "@cash", :ammount => 3500, :description => "to pay coworking space" }
+      end
+
+      it "should recognize the expression without description" do
+        parts = Grammatic.parse("@savings @cash 3500")
+        parts.should == { :from => "@savings", :to => "@cash", :ammount => 3500 }
       end
     end
   end
