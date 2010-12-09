04f13d93871a4ffdadf4c54efc6fe34fbfe0ce3f
Bernardo Castro <bernacas@gmail.com>
2010-12-09 12:22:10 -0200

    Refactor grammatic lib

--- lib/grammatic.rb
+++ lib/grammatic.rb
@@ -10,7 +10,8 @@ module Grammatic
   # Raised when you pass a different element type and can't be recognized
   class CantReturnFormattedOutput < StandardError; end
 
-  module Helpers
+
+  module Grammar
     # Helper method to return a formatted text_value
     def format_output(result, type = :string)
       raise InvalidElement, "can't be a nil element" if result.nil?
@@ -30,40 +31,40 @@ module Grammatic
     def has_description?(result)
       result.respond_to?('description') && !result.description.text_value.blank?
     end
-  end
-
-  module Grammar
-    include Grammatic::Helpers
 
     # All valid expressions are transactions
     class Transaction < Treetop::Runtime::SyntaxNode
+      def to_json
+        return { :description => format_output(description) } if has_description?(self)
+        {}
+      end
     end
 
     # Parse a expression which belongs to a expense rule
     class Expense < Transaction
       def to_json
-        { :account => format_output(expense.account), :tags => format_output(expense.tags), :amount => format_output(expense.debit, :integer) }
+        super.merge({ :account => format_output(expense.account), :tags => format_output(expense.tags), :amount => format_output(expense.debit, :integer) })
       end
     end
 
     # Parse a expression which belongs to a deposit rule
     class Deposit < Transaction
       def to_json
-        { :account => format_output(deposit.account), :tags => format_output(deposit.tags), :amount => format_output(deposit.credit, :integer) }
+        super.merge({ :account => format_output(deposit.account), :tags => format_output(deposit.tags), :amount => format_output(deposit.credit, :integer) })
       end
     end
 
     # Parse a expression which belongs to a transfer rule
     class Transfer < Transaction
       def to_json
-        { :from => format_output(transfer.from), :to => format_output(transfer.to), :ammount => format_output(transfer.amount, :integer) }
+        super.merge({ :from => format_output(transfer.from), :to => format_output(transfer.to), :ammount => format_output(transfer.amount, :integer) })
       end
     end
 
     # Parse a expression which belongs to a exchange rule
     class Exchange < Transaction
       def to_json
-        { :from => format_output(exchange.from), :to => format_output(exchange.to), :sell => format_output(exchange.sell, :integer), :buy => format_output(exchange.buy, :integer) }
+        super.merge({ :from => format_output(exchange.from), :to => format_output(exchange.to), :sell => format_output(exchange.sell, :integer), :buy => format_output(exchange.buy, :integer) })
       end
     end
   end
@@ -82,8 +83,6 @@ module Grammatic
 
     raise InvalidExpression, "The expression is not valid" if result.nil?
 
-    parts = result.to_json
-    parts[:description] = format_output(result.description) if has_description?(result)
-    parts
+    result.to_json
   end
 end
