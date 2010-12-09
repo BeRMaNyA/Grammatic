4e396069c9925c96cee4eb548d3fe21a1cb3c8cd
Bernardo Castro <bernacas@gmail.com>
2010-12-09 15:08:19 -0200

    Refactor code
    * We are returning a hash not a json, all references were changed
    * Add transaction's type in the hash

--- lib/grammatic.rb
+++ lib/grammatic.rb
@@ -4,9 +4,6 @@ module Grammatic
   # Raised when you enter an invalid expression
   class InvalidExpression < StandardError; end
 
-  # Raised when you pass an invalid element to format the output
-  class InvalidElement < StandardError; end
-
   # Raised when you pass a different element type and can't be recognized
   class CantReturnFormattedOutput < StandardError; end
 
@@ -14,8 +11,6 @@ module Grammatic
   module Grammar
     # Helper method to return a formatted text_value
     def format_output(result, type = :string)
-      raise InvalidElement, "can't be a nil element" if result.nil?
-
       text_value = result.text_value
 
       return case type
@@ -23,9 +18,9 @@ module Grammatic
           text_value.squeeze(" ").strip
         when :integer
           text_value.to_i
+        else
+          raise CantReturnFormattedOutput, "can't return a formatted output"
         end
-
-      raise CantReturnFormattedOutput, "can't return a formatted output"
     end
 
     # Helper method to detect if the element has description
@@ -35,7 +30,7 @@ module Grammatic
 
     # All valid expressions are transactions
     class Transaction < Treetop::Runtime::SyntaxNode
-      def to_json
+      def to_hash
         return { :description => format_output(description) } if has_description?(self)
         {}
       end
@@ -43,34 +38,34 @@ module Grammatic
 
     # Parse a expression which belongs to a expense rule
     class Expense < Transaction
-      def to_json
-        super.merge({ :account => format_output(expense.account), :tags => format_output(expense.tags), :amount => format_output(expense.debit, :integer) })
+      def to_hash
+        super.merge({ :type => "expense", :account => format_output(expense.account), :tags => format_output(expense.tags), :amount => format_output(expense.debit, :integer) })
       end
     end
 
     # Parse a expression which belongs to a deposit rule
     class Deposit < Transaction
-      def to_json
-        super.merge({ :account => format_output(deposit.account), :tags => format_output(deposit.tags), :amount => format_output(deposit.credit, :integer) })
+      def to_hash
+        super.merge({ :type => "deposit", :account => format_output(deposit.account), :tags => format_output(deposit.tags), :amount => format_output(deposit.credit, :integer) })
       end
     end
 
     # Parse a expression which belongs to a transfer rule
     class Transfer < Transaction
-      def to_json
-        super.merge({ :from => format_output(transfer.from), :to => format_output(transfer.to), :ammount => format_output(transfer.amount, :integer) })
+      def to_hash
+        super.merge({ :type => "transfer", :from => format_output(transfer.from), :to => format_output(transfer.to), :ammount => format_output(transfer.amount, :integer) })
       end
     end
 
     # Parse a expression which belongs to a exchange rule
     class Exchange < Transaction
-      def to_json
-        super.merge({ :from => format_output(exchange.from), :to => format_output(exchange.to), :sell => format_output(exchange.sell, :integer), :buy => format_output(exchange.buy, :integer) })
+      def to_hash
+        super.merge({ :type => "exchange", :from => format_output(exchange.from), :to => format_output(exchange.to), :sell => format_output(exchange.sell, :integer), :buy => format_output(exchange.buy, :integer) })
       end
     end
   end
 
-  # Return a json with the parts of the expression or
+  # Return a hash with the parts of the expression or
   # raise an exception if the expression is not valid
   #
   # Grammatic.parse("@savings super food 100 some groceries for tonight")
@@ -84,6 +79,6 @@ module Grammatic
 
     raise InvalidExpression, "The expression is not valid" if result.nil?
 
-    result.to_json
+    result.to_hash
   end
 end
--- spec/grammatic_spec.rb
+++ spec/grammatic_spec.rb
@@ -5,55 +5,101 @@ describe "Grammatic" do
     @parser = BudgeteerGrammarParser.new
   end
 
-  describe Expense do
-    describe "giving a valid expression" do
-      it "should recognize all the parts of that string" do
-        parts = Grammatic.parse("@savings super food 100 some groceries for tonight")
-        parts.should == { :tags => "super food", :amount => 100, :account => "@savings", :description => "some groceries for tonight" }
+  describe "Grammar" do
+    describe Expense do
+      describe "giving a valid expression" do
+        it "should recognize all the parts of that string" do
+          expense = @parser.parse("@savings super food 100 some groceries for tonight")
+          expense.to_hash.should == { :type => "expense", :tags => "super food", :amount => 100, :account => "@savings", :description => "some groceries for tonight" }
+        end
+
+        it "should regognize minimal correct expression" do
+          expense = @parser.parse("beer 55")
+          expense.to_hash.should == { :type => "expense", :tags => "beer", :amount => 55, :account => "" }
+        end
+      end
+    end
+
+    describe Deposit do
+      describe "giving a valid expression" do
+        it "should recognize all the parts of that string" do
+          deposit = @parser.parse("@savings salary +1000 august income")
+          deposit.to_hash.should == { :type => "deposit", :account => "@savings", :tags => "salary", :amount => 1000, :description => "august income" }
+        end
+      end
+    end
+
+    describe Transfer do
+      describe "giving a valid expression" do
+        it "should recognize all the parts of that string" do
+          transfer = @parser.parse("@savings @cash 3500 to pay coworking space")
+          transfer.to_hash.should == { :type => "transfer", :from => "@savings", :to => "@cash", :ammount => 3500, :description => "to pay coworking space" }
+        end
+
+        it "should recognize the expression without description" do
+          transfer = @parser.parse("@savings @cash 3500")
+          transfer.to_hash.should == { :type => "transfer", :from => "@savings", :to => "@cash", :ammount => 3500 }
+        end
+      end
+    end
+
+    describe Exchange do
+      describe "giving a valid expression" do
+        it "should recognize all the parts of that string" do
+          exchange = @parser.parse("@savings_in_dollars @savings_in_pesos 100 2225 so we have some pesos to spend")
+          exchange.to_hash.should == { :type => "exchange", :from => "@savings_in_dollars", :to => "@savings_in_pesos", :sell => 100, :buy => 2225, :description => "so we have some pesos to spend" }
+        end
+      end
+    end
+
+    describe "calling the helper format_output" do
+      it "should format the output for a given element" do
+        treetop_syntax = @parser.parse("@savings stadium soccer penarol 120 pay ticket to watch my team")
+
+        # Strings
+        "stadium soccer penarol ".should == treetop_syntax.expense.tags.text_value
+        "stadium soccer penarol".should   == Grammatic.format_output(treetop_syntax.expense.tags)
+
+        # Integers
+        "120 ".should == treetop_syntax.expense.debit.text_value
+        120.should    == Grammatic.format_output(treetop_syntax.expense.debit, :integer)
       end
 
-      it "should regognize minimal correct expression" do
-        parts = Grammatic.parse("beer 55")
-        parts.should == { :tags => "beer", :amount => 55, :account => "" }
+      it "should catch exceptions when the element is not valid" do
+        treetop_valid_syntax = @parser.parse("@savings stadium soccer penarol 120 pay ticket to watch my team")
+
+        # passing invalid type to format
+        lambda do
+          Grammatic.format_output(treetop_valid_syntax.expense.debit, :another_thing)
+        end.should raise_error(Grammatic::CantReturnFormattedOutput)
+
+        # passing nil element
+        treetop_invalid_syntax = @parser.parse("invalid sintax")
+        lambda do
+          Grammatic.format_output(treetop_invalid_syntax.description)
+        end.should raise_error(NoMethodError)
       end
+    end
+
+    it "should return true if the element has description" do
+      treetop_syntax_with_description = @parser.parse("@savings stadium soccer penarol 120 pay ticket to watch my team")
+      Grammatic.has_description?(treetop_syntax_with_description).should be_true
+
+      treetop_syntax_without_description = @parser.parse("@savings stadium soccer penarol 120")
+      Grammatic.has_description?(treetop_syntax_without_description).should be_false
     end
   end
 
-  describe Deposit do
-    describe "giving a valid expression" do
-      it "should recognize all the parts of that string" do
-        parts = Grammatic.parse("@savings salary +1000 august income")
-        parts.should == { :account => "@savings", :tags => "salary", :amount => 1000, :description => "august income" }
-      end
+  describe "calling the method parse" do
+    it "should return a hash" do
+      result = Grammatic.parse("@savings stadium soccer penarol 120 pay ticket to watch my team")
+      result.should == { :type => "expense", :account => "@savings", :tags => "stadium soccer penarol", :amount => 120, :description => "pay ticket to watch my team" }
     end
-  end
 
-  describe Transfer do
-    describe "giving a valid expression" do
-      it "should recognize all the parts of that string" do
-        parts = Grammatic.parse("@savings @cash 3500 to pay coworking space")
-        parts.should == { :from => "@savings", :to => "@cash", :ammount => 3500, :description => "to pay coworking space" }
-      end
-
-      it "should recognize the expression without description" do
-        parts = Grammatic.parse("@savings @cash 3500")
-        parts.should == { :from => "@savings", :to => "@cash", :ammount => 3500 }
-      end
+    it "should catch exceptions" do
+      lambda do
+        Grammatic.parse("invalid expression")
+      end.should raise_error(Grammatic::InvalidExpression)
     end
-  end
-
-  describe Exchange do
-    describe "giving a valid expression" do
-      it "should recognize all the parts of that string" do
-        parts = Grammatic.parse("@savings_in_dollars @savings_in_pesos 100 2225 so we have some pesos to spend")
-        parts.should == { :from => "@savings_in_dollars", :to => "@savings_in_pesos", :sell => 100, :buy => 2225, :description => "so we have some pesos to spend" }
-      end
-    end
-  end
-
-  it "should catch exceptions while proccess the expressions" do
-    lambda do
-      Grammatic.parse("invalid expression")
-    end.should raise_error(Grammatic::InvalidExpression)
   end
 end
